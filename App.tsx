57cc5e49bb924a8b9c26f906fd0041f742d3809e
박민 <zooqzqz@gmail.com>
2019-05-19 01:38:51 +0900

    ENH: code push

--- App.tsx
+++ App.tsx
@@ -2,14 +2,23 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  *
+ * Generated with the TypeScript template
+ * https://github.com/emin93/react-native-template-typescript
+ *
  * @format
- * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from "react";
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from "react-native";
 
 import Reactotron from "reactotron-react-native";
+import CodePush from "react-native-code-push";
 
 import { reactotronSettingsUp } from "./reactotronSettings";
 import { sentrySettingsUp } from "./sentrySettings";
@@ -21,21 +30,30 @@ reactotronSettingsUp();
 sentrySettingsUp();
 
 const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+    "Double tap R on your keyboard to reload,\n" +
+    "Shake or press menu button for dev menu"
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+interface Props {}
+class App extends Component<Props> {
+  private onButtonPress = () => {
+    CodePush.sync({
+      updateDialog: CodePush.DEFAULT_UPDATE_DIALOG,
+      installMode: CodePush.InstallMode.IMMEDIATE
+    });
+  };
   render() {
     Reactotron.log("ReactApp Start!!");
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
+        <Text style={styles.welcome}>Welcome to CodePushApp!!!</Text>
+        <Text style={styles.instructions}>To get started, edit App.tsx</Text>
         <Text style={styles.instructions}>{instructions}</Text>
+        <TouchableOpacity onPress={this.onButtonPress}>
+          <Text>Check for updates</Text>
+        </TouchableOpacity>
       </View>
     );
   }
@@ -44,18 +62,26 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#F5FCFF"
   },
   welcome: {
     fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+    textAlign: "center",
+    margin: 10
   },
   instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    textAlign: "center",
+    color: "#333333",
+    marginBottom: 5
+  }
 });
+
+const codePushOptions = {
+  checkFrequency: CodePush.CheckFrequency.ON_APP_START
+};
+
+const CodePushApp = CodePush(codePushOptions)(App);
+
+export default CodePushApp;
